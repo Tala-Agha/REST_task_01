5153c9eb0b1e750161f6ecc304b65aa606727269
Tala Agha <macbook@MacBooks-MacBook-Pro.local>
2020-02-23 15:03:12 +0200

    DRFView

--- flights/views.py
+++ flights/views.py
@@ -1,1 +1,13 @@
 from .models import Flight, Booking
+from rest_framework.generics import ListAPIView
+from .serializers import FlightSerializer,BookingSerializer
+from datetime import datetime
+
+
+class Flights(ListAPIView):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+class Bookings(ListAPIView):
+    queryset = Booking.objects.filter(date__gt = datetime.today())
+    serializer_class = BookingSerializer
--- task_1/urls.py
+++ task_1/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from flights import views
+from flights.views import ListAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('list/flights',views.Flights.as_view(),name='flights-list'),
+    path('list/bookings',views.Bookings.as_view(),name='bookings-list'),
+
 ]
